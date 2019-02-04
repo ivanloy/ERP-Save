eb83d4ae2ad19d91b024fcfa923b3a1585f8e908
jolsensei <horusensei@gmail.com>
2019-02-04 12:51:19 +0100

    Chips and nice colors

--- Unificacion Proyecto (Jorge)/FlamERPenny/src/app/service/clients.service.ts
+++ Unificacion Proyecto (Jorge)/FlamERPenny/src/app/service/clients.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Client } from './Client';
+import { Client } from '../models/Client';
 
 @Injectable({
   providedIn: 'root'
